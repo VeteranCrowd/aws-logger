278e05a2d14706dc4e0c3f2a59f8c14c2173be5a
Jason Williscroft <me@karmanivero.us>
2024-07-24 00:55:26 +0800

    updated dependency

--- src/getLogger.test.ts
+++ src/getLogger.test.ts
@@ -1,7 +1,21 @@
+import { type APIGatewayProxyEvent, type Context } from 'aws-lambda';
 import { expect } from 'chai';
 
+import { getLogger } from './getLogger';
+
 describe('getLogger', function () {
-  it('should pass', function () {
-    expect(true).to.be.true;
+  it('should initialize', function () {
+    expect(async () => {
+      await getLogger(
+        {
+          bucket: 'foo',
+          logLevel: 'debug',
+          roleArn: 'bar',
+          roleSessionName: 'baz',
+        },
+        {} as APIGatewayProxyEvent,
+        {} as Context,
+      );
+    }).not.to.throw();
   });
 });
